9d425b2438d1f0206bc8459a86f48e0815506368
Luay Younus <luayghazwan@yahoo.com>
2017-06-16 00:21:36 -0700

    Player Class with Private properties

---
+++ src/main/java/org/LibraryPOS/maven/Player.java
@@ -0,0 +1,11 @@
+package org.LibraryPOS.maven;
+
+/**
+ * Created by luayghazwan on 6/16/17.
+ */
+public class Player {
+    private String playerName;
+    private int playerNumber;
+    private String sportType;
+    private int yearsOfExperience;
+}
