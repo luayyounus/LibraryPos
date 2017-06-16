82e7d0b141289bea5462bbdac5894938e5b69e55
Luay Younus <luayghazwan@yahoo.com>
2017-06-16 15:26:35 -0700

    Sport Class with default instructor implemented

---
+++ src/main/java/org/LibraryPOS/maven/Sport.java
@@ -0,0 +1,17 @@
+package org.LibraryPOS.maven;
+
+/**
+ * Created by luayghazwan on 6/16/17.
+ */
+public class Sport {
+
+    public SportType sportType;
+
+    public Sport(){
+        super();
+    }
+    public Sport(SportType sport){
+        this.sportType = sport;
+    }
+
+}
