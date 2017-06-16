fcb61485c011867d53d733eb4f920e49612c772c
Luay Younus <luayghazwan@yahoo.com>
2017-06-16 15:43:15 -0700

    Player extends Team with properties and overridden match & toString methods

--- src/main/java/org/LibraryPOS/maven/Player.java
+++ src/main/java/org/LibraryPOS/maven/Player.java
@@ -3,11 +3,11 @@ package org.LibraryPOS.maven;
 /**
  * Created by luayghazwan on 6/16/17.
  */
-public class Player {
-    private String playerName;
-    private int playerNumber;
-    private String sportType;
-    private int yearsOfExperience;
+public class Player extends Team {
+    private String playerName, sportType;
+    private int playerNumber, yearsOfExperience;
+
+    Tennis tennis;
 
     //Default Constructor with Default Values
     public Player () {
@@ -23,5 +23,20 @@ public class Player {
         this.playerNumber = playerNumber;
         this.sportType = sportType;
         this.yearsOfExperience = yearsOfExperience;
+    }
+
+    @Override
+    void matchWith(Player newPlayer){
+        System.out.println("Match started!");
+    }
+
+    @Override
+    void matchPlan () {
+        System.out.println("The plan is 4-3-3 defense for the first 30 mins.");
+    }
+
+    @Override
+    public String toString() {
+        return playerName + " | " + playerNumber + " | " + sportType + " | " + yearsOfExperience + " years of experience.";
     }
 }
