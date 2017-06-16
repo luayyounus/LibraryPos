31e7738e5f637b1c41eb03cb2a6540fd986963a2
Luay Younus <luayghazwan@yahoo.com>
2017-06-16 15:37:01 -0700

    Enum for sports types

---
+++ src/main/java/org/LibraryPOS/maven/SportType.java
@@ -0,0 +1,16 @@
+package org.LibraryPOS.maven;
+
+/**
+ * Created by luayghazwan on 6/16/17.
+ */
+
+public enum SportType {
+    Tennis,
+    Football,
+    Baseball,
+    Basketball,
+    Rugby,
+    BrazilianFutsal,
+    Badminton,
+    Swimming;
+}
