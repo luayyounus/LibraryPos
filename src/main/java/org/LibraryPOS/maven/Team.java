ea6dacb71130313715f9db217abd88dacfd4b8bd
Luay Younus <luayghazwan@yahoo.com>
2017-06-16 15:24:56 -0700

    Team class implements League Interface methods: Rules and Type

--- src/main/java/org/LibraryPOS/maven/Team.java
+++ src/main/java/org/LibraryPOS/maven/Team.java
@@ -3,14 +3,25 @@ package org.LibraryPOS.maven;
 /**
  * Created by luayghazwan on 6/16/17.
  */
-public abstract class Team {
-    abstract void matchWith (String teamName);
+public abstract class Team implements League {
+    private String teamName;
+    private int teamSize;
 
-    private void matchPlan () {
-        System.out.println("Make a plan for this match to win the game.");
-    }
+    abstract void matchWith (Player player);
+    abstract void matchPlan ();
 
     public void calculateScore () {
         System.out.println("Implement a score calculation for your Game!");
+    }
+
+    public String getTeamName(){
+        return teamName;
+    }
+
+    public String leagueRules(){
+        return "Make sure the green ball pass over the net when served.";
+    }
+    public String leagueType(){
+        return "Gold";
     }
 }
