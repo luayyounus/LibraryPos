01a046213f5691c611d1d0525e54e70245d35a3c
Luay Younus <luayghazwan@yahoo.com>
2017-06-16 15:46:51 -0700

    Creating class instances, calling methods and setting values in the main function

--- src/main/java/org/LibraryPOS/maven/App.java
+++ src/main/java/org/LibraryPOS/maven/App.java
@@ -8,6 +8,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        SportFactory sportFactory;
+
+        Equipment equipment = new Equipment();
+        equipment.addRacquets(5);
+
+        Player player1 = new Player("Ronaldo", 6, "Tennis", 10);
+        Player player2 = new Player("Luay", 7, "Tennis",3);
+
+        player1.matchWith(player2);
+
+        player1.tennis.setScore(15);
+        player2.tennis.setScore(15);
+
+        player1.tennis.getResult();
     }
 }
